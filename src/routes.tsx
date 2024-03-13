385bca25cbb49a09be90b26b53214c2eff3885fb
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2024-03-13 09:35:26 -0300

    Configuring routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,29 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboards'
 import { SignIn } from './pages/auth/sign-in'
 
 export const Router = createBrowserRouter([
   {
     path: '/',
-    element: <Dashboard />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Dashboard />,
+      },
+    ],
   },
   {
-    path: '/auth',
-    element: <SignIn />,
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/auth',
+        element: <SignIn />,
+      },
+    ],
   },
 ])
